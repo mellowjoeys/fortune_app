f9cc4f5a8787e79fba49ccef9746dd4ed559b8b4
mellowjoeys <mellowjoey@gmail.com>
2019-08-27 11:24:58 -0500

    fortune app functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get '/rand_fortune_url' => 'my_examples#rand_fortune_action'
+    get '/lotto_numbers_url' => 'my_examples#lotto_nums_action'
+    # get 'hopeless_count' => 'hopeless_count.json.jb'
+    get '/99_bottles_url' => 'my_examples#lyrics_action'
+  end
 end
